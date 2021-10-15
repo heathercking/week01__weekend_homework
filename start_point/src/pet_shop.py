7059d6ffebb5674f81a41047549a69530bbe8731
Heather King <hkingdevelopment@gmail.com>
2021-10-15 13:03:23 +0100

    function #2 written and passed

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -4,4 +4,8 @@ import pdb
 def get_pet_shop_name(dictionary):
     pet_shop_name = dictionary["name"]
     return pet_shop_name
-    
+
+def get_total_cash(dictionary):
+    total_cash = dictionary["admin"]["total_cash"]
+    print(total_cash)
+    return total_cash
