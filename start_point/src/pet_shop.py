ac4cd007d4507c884b0da686514191549440d643
Heather King <hkingdevelopment@gmail.com>
2021-10-15 13:41:37 +0100

    function #3 written and passed

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -7,5 +7,8 @@ def get_pet_shop_name(dictionary):
 
 def get_total_cash(dictionary):
     total_cash = dictionary["admin"]["total_cash"]
-    print(total_cash)
     return total_cash
+
+def add_or_remove_cash(dictionary, amount):
+    dictionary["admin"]["total_cash"] += amount
+
